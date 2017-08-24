2717739c7355da819343af8a63b9ecefded5ff52
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2017-08-24 10:08:29 -0400

    update sw

--- me/src/sw.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-self.addEventListener('install', (event)=>{
-  console.log('From SW: Install Event', event);
-});
-
-self.addEventListener('activate', (event)=>{
-  console.log('From SW: Activate Event', event);
-});
--- me/src/swRegister.js
+++
@@ -1,9 +0,0 @@
-// Check the compatibility and Register our service worker
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-  .then((swRegistration)=>{
-    console.log('swRegistration', swRegistration);
-  }).catch((error)=>{
-    console.log( error);
-  })
-}
